7b306105b90c3b5109077ea20e55c05759c3ccda
Sebastian Peters <Sebastian.Peters@gmail.com>
2016-04-17 20:18:16 +0200

    Provide new graphics for cumulated running distance
    * Extract method for finishSeries and fix corner cases
    * Encapsulate some graph vars in graphs object
    * Replace reset comment with logging

--- src/main/resources/public/js/graphs.controller.js
+++ src/main/resources/public/js/graphs.controller.js
@@ -64,81 +64,107 @@
       vm.distanceData = [];
       vm.distanceLabels = [];
       vm.distanceSeriesLabels = [];
+      vm.cumulatedDistanceData = [];
 
       if (_.isEmpty(activities)) {
-        // reset graphics canvas
+        logger.debug('reset distance graphics canvas');
         vm.distanceData.push(0);
         vm.distanceSeriesLabels.push(0);
         return;
       }
 
-      const sortedActivities = _.sortBy(activities, CONST.DATE);
+      var graphs = {
+        'groupBy': groupBy,
+        'multipleSeries': vm.filterYear.key === CONST.KEY_ALL && groupBy === KM_PER_MONTH,
+        'data': {},
+        'labels': {},
+        'cumulatedData': {},
+        'cumulatedDistance': 0
+      };
 
-      let data = {};
-      let labels = {};
-
-      const multipleSeries = vm.filterYear.key === CONST.KEY_ALL && groupBy === KM_PER_MONTH;
-      if (multipleSeries) {
-        for (let month = 1; month <= 12; month++) {
-          vm.distanceLabels.push(`${(month < 10 ? '0' : '')}${month}`);
+      if (graphs.multipleSeries) {
+        for (var month = 1; month <= 12; month++) {
+          vm.distanceLabels.push((month < 10 ? '0' : '') + month);
         }
       }
 
-      for (let item = 0, len = sortedActivities.length; item < len; item++) {
-        const readableDateLong = utilService.dateFilter(sortedActivities[item].date).split(DASH);
-        const readableDateShort = utilService.dateFilter(sortedActivities[item].date).split(DASH);
+      processActivities(activities, graphs);
+    }
+
+    function processActivities(activities, graphs) {
+      activities = _.sortBy(activities, CONST.DATE);
+      for (var i = 0, len = activities.length; i < len; i++) {
+        var readableDateLong = utilService.dateFilter(activities[i].date).split(DASH);
+        var readableDateShort = utilService.dateFilter(activities[i].date).split(DASH);
 
         let label;
         let groupByKey;
         let year;
 
-        if (multipleSeries) {
+        if (graphs.multipleSeries) {
           label = readableDateLong[1];
           groupByKey = label;
           year = readableDateLong[0];
+          vm.distanceSeriesLabels.push(year);
         } else {
-          if (groupBy === KM_PER_YEAR) {
+          if (graphs.groupBy === KM_PER_YEAR) {
             label = readableDateLong[0];
             groupByKey = label;
-          } else if (groupBy === KM_PER_MONTH) {
+          } else if (graphs.groupBy === KM_PER_MONTH) {
             label = readableDateLong[1] + DASH + readableDateLong[0];
             groupByKey = label;
-          } else if (groupBy === KM_PER_DAY) {
+          } else if (graphs.groupBy === KM_PER_DAY) {
             label = readableDateShort[2] + DOT + readableDateShort[1];
             groupByKey = label + DOT + readableDateShort[0];
           }
         }
-        const distance = parseFloat(sortedActivities[item].distance.toFixed(2));
+        var distance = parseFloat(activities[i].distance.toFixed(2));
 
-        data[groupByKey] = data[groupByKey] + distance || distance;
-        labels[groupByKey] = labels[groupByKey] = label;
+        graphs.data[groupByKey] = graphs.data[groupByKey] + distance || distance;
+        graphs.labels[groupByKey] = label;
+        graphs.cumulatedDistance += distance;
+        graphs.cumulatedData[groupByKey] = graphs.cumulatedDistance;
 
-        if (item + 1 === len || year && year !== utilService.dateFilter(sortedActivities[item + 1].date)
-            .split(DASH)[0]) {
-          logger.debug('end of series');
-
-          const series = [];
-          if (multipleSeries) {
-            vm.distanceSeriesLabels.push(year);
-            for (const monthKey in vm.distanceLabels) {
-              let valueForMonth = data[vm.distanceLabels[monthKey]];
-              valueForMonth = !isNaN(valueForMonth) ? parseFloat(valueForMonth).toFixed(2) : 0;
-              series.push(valueForMonth);
-            }
-          } else {
-            for (const key in labels) {
-              series.push(data[key]);
-              vm.distanceLabels.push(labels[key]);
-            }
-          }
-
-          // need array within array because series allows multiple lines -> multiple data-arrays in main data-array
-          vm.distanceData.push(series);
-
-          data = {};
-          labels = {};
+        if (i + 1 === len || year && year !== utilService.dateFilter(activities[i + 1].date).split(DASH)[0]) {
+          finishSeries(graphs);
         }
       }
+    }
+
+    function finishSeries(graphs) {
+      logger.debug('finishSeries');
+      var series = [];
+      var cumulatedSeries = [];
+
+      if (graphs.multipleSeries) {
+        for (var monthKey in vm.distanceLabels) {
+          var seriesKey = vm.distanceLabels[monthKey];
+          series.push(graphs.data[seriesKey] || 0);
+
+          var distanceOfMonth = graphs.cumulatedData[seriesKey];
+          if (!distanceOfMonth) {
+            // take cumulated distance till now if previous month is defined, otherwise 0
+            var prevSeriesKey = prevSeriesKey || vm.distanceLabels[monthKey - 1];
+            distanceOfMonth = graphs.data[prevSeriesKey] ? graphs.cumulatedDistance : 0;
+          }
+          cumulatedSeries.push(distanceOfMonth);
+        }
+      } else {
+        for (var key in graphs.labels) {
+          series.push(graphs.data[key]);
+          cumulatedSeries.push(graphs.cumulatedData[key]);
+          vm.distanceLabels.push(graphs.labels[key]);
+        }
+      }
+
+      // need array within array because series allows multiple lines -> multiple data-arrays in main data-array
+      vm.distanceData.push(series);
+      vm.cumulatedDistanceData.push(cumulatedSeries);
+
+      graphs.data = {};
+      graphs.labels = {};
+      graphs.cumulatedDistance = 0;
+      graphs.cumulatedData = {};
     }
 
     function renderDistributionChart(activities) {
@@ -147,7 +173,7 @@
       vm.distributionLabels = [];
 
       if (_.isEmpty(activities)) {
-        // reset graphics canvas
+        logger.debug('reset distribution graphics canvas');
         vm.distributionData.push(0);
         vm.distributionLabels.push('No data');
         return;
